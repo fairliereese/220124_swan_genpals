6eacd2bdea373410984e07c2bbcb27c5ea4a01b5
fairliereese <fairliek@comcast.net>
2022-01-27 11:12:13 -0800

    removed path finder thingie

--- swan_die.sh
+++ swan_die.sh
@@ -4,5 +4,4 @@ files=$1
 dos2unix $files
 
 n=`wc -l $files | cut -d' ' -f1`
-d="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/"
-sbatch --array=1-${n} ${d}sbatch_swan_die.sh $files
+sbatch --array=1-${n} sbatch_swan_die.sh $files
